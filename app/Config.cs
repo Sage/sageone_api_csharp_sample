a8a2effea2a3e121d14abd80ad1a68b70f23e111
“Robert <robert.schmidt@sage.com>
2019-07-10 16:37:53 +0200

    add config template and exclude local config file

---
+++ app/Config.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace app
+{
+    public class Config
+    {
+        public const string ClientId = "YOUR_CLIENT_ID";
+
+        public const string ClientSecret = "YOUR_CLIENT_SECRET";
+
+        public const string CallbackUrl = "http://localhost:8080/auth/callback/";
+    }
+}
